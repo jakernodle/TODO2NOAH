dc6195721738e27eeb22375008b7e6b596c67225
JA <jakernodle@gmail.com>
2024-04-03 12:39:16 -0600

    Initial Commit

---
+++ TODO2POO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TODO2POO
+//
+//  Created by JohnAnge Kernodle on 4/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TODO2POO/TODO2POOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODO2POOApp.swift
+//  TODO2POO
+//
+//  Created by JohnAnge Kernodle on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TODO2POOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
